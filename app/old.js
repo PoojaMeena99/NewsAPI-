3ab918b20687d3dd32ba307993da61fa9987ac4a
Pooja Meena <poojameena21@navgurukul.org>
2024-04-03 15:01:13 +0530

    Displayed 10 articles from API

---
+++ app/old.js
@@ -0,0 +1,5 @@
+// it is use for sliceing
+
+// let limitedArray = articleArray.slice(0, 10);
+//         let limit=limitedArray
+        // console.log(limit,"**10***")
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from "react";
 import Header from "./header";
 import Searchbar from "./searchbar";
 import Grid from "./grid";
@@ -6,44 +7,44 @@ import Footer from "./footer";
 import articles from "./article_data";
 
 function page() {
-  
-  const fetchData = function(input) {
+  const [changeValue, setchangeValue] = useState(articles);
+  const articleArray = [];
+
+  const fetchData = function (input) {
     console.log("start fatchinggggg...");
     fetch(
-      `https://newsapi.org/v2/everything?q=${input}&sortBy=publishedAt&apiKey=87b8452aff0942a3abff56c30c065893`
+      `https://newsapi.org/v2/everything?q=${input}&sortBy=publishedAt&pageSize=10&apiKey=87b8452aff0942a3abff56c30c065893`
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data, "response");
         const articlesData = data.articles;
 
-        const articleArray = [];
-
         for (let i = 0; i < articlesData.length; i++) {
           const article = articlesData[i];
           console.log(article, "article[i]");
 
           const articleObject = {
+            image: article.urlToImage,
             title: article.title,
             description: article.description,
-            image: article.urlToImage,
             url: article.url,
           };
-
           articleArray.push(articleObject);
         }
+        console.log(articleArray, "***");
+        setchangeValue(articleArray);
       });
-  }
+  };
+
   return (
     <>
       <Header />
-      <Searchbar fetchData = {fetchData} />
-      <Grid articles={articles} />
+      <Searchbar fetchData={fetchData} />
+      <Grid articles={changeValue} />
       <Footer />
     </>
   );
 }
 
 export default page;
-
-
